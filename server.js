0b58f4f1237cca81361ff6a24ab76ac866766643
khaledshaabanbasha <102066445+khaledshaabanbasha@users.noreply.github.com>
2024-02-18 21:34:16 +0200

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const io = socketIO(server);
 
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static('/'));
+app.use(express.static('../'));
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
